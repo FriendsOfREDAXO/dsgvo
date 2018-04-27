9669c021bec4be9a0091ad06af1d4a66397801cc
Alexander Walther <github.com@alexplus.de>
2018-04-27 15:21:43 +0200

    PHP-Mailer-Logs Verzeichnis auslesen

--- pages/dsgvo.setup.phpmailer.php
+++ pages/dsgvo.setup.phpmailer.php
@@ -22,39 +22,45 @@ if (rex::getUser()->isAdmin()) {
         $pre.= "-";
         $dh = opendir($dir);
         while($file = readdir($dh)) {
-          if($file != "." && $file != "..") {
-            if(is_dir("$dir/$file")) {
-              dump("$pre $file [DIR]");
-              getDir("$dir/$file",$pre);
-            } else {
-              $files[] = $file;
+            if($file != "." && $file != "..") {
+                if(is_dir("$dir/$file")) {
+                    // dump("$pre $file [DIR]");
+                    getDir("$dir/$file",$pre);
+                } else {
+                    //dump("$pre $file [DIR]");
+                    $files[$dir] = $file;
+                }
             }
-          }
         }
         closedir($dh);
+        return $files;
       }
     
+    $files = getDir($dir, "");
+
     $content = '<table class="table table-striped table-hover">
     <thead>
         <tr>
             <th class="rex-table-icon"></th>
-            <th>' . rex_i18n::msg('backup_filename') . '</th>
-            <th class="rex-table-width-5">' . rex_i18n::msg('backup_filesize') . '</th>
-            <th class="rex-table-width-5">' . rex_i18n::msg('backup_createdate') . '</th>
+            <th>' . rex_i18n::msg('check_dsgvo_phpmailer-logs_filename') . '</th>
+            <th class="rex-table-width-5">' . rex_i18n::msg('check_dsgvo_phpmailer-logs_folder') . '</th>
+            <th class="rex-table-width-5">' . rex_i18n::msg('check_dsgvo_phpmailer-logs_filesize') . '</th>
+            <th class="rex-table-width-5">' . rex_i18n::msg('check_dsgvo_phpmailer-logs_createdate') . '</th>
         </tr>
     </thead>
     <tbody>';
 
-    foreach ($files as $file) {
-        $filec = date('d.m.Y H:i', filemtime($filepath));
-        $filesize = rex_file::formattedSize($filepath);
+    foreach ($files as $folder => $file) {
+        $filec = date('d.m.Y H:i', filemtime($folder."/".$file));
+        $filesize = rex_file::formattedSize($folder."/".$file);
 
 
         $content .= '<tr>
-                        <td class="rex-table-icon"><i class="rex-icon rex-icon-database"></i></td>
-                        <td data-title="' . rex_i18n::msg('backup_filename') . '">' . $file . '</td>
-                        <td data-title="' . rex_i18n::msg('backup_filesize') . '">' . $filesize . '</td>
-                        <td data-title="' . rex_i18n::msg('backup_createdate') . '">' . $filec . '</td>
+                        <td class="rex-table-icon"><i class="rex-icon rex-icon-article"></i></td>
+                        <td data-title="' . rex_i18n::msg('check_dsgvo_phpmailer-logs_filename') . '">' . $file . '</td>
+                        <td data-title="' . rex_i18n::msg('check_dsgvo_phpmailer-logs_folder') . '">' . $folder . '</td>
+                        <td data-title="' . rex_i18n::msg('check_dsgvo_phpmailer-logs_filesize') . '">' . $filesize . '</td>
+                        <td data-title="' . rex_i18n::msg('check_dsgvo_phpmailer-logs_createdate') . '">' . $filec . '</td>
                     </tr>
         ';
     }
@@ -64,7 +70,7 @@ if (rex::getUser()->isAdmin()) {
                     </table>';
     
     $fragment = new rex_fragment();
-    $fragment->setVar('title', rex_i18n::msg('backup_export_db_caption'), false);
+    $fragment->setVar('title', rex_i18n::msg('check_dsgvo_phpmailer-logs_caption'), false);
     $fragment->setVar('content', $content, false);
     $content = $fragment->parse('core/page/section.php');
     
