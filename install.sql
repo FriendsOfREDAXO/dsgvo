26fa83ccb836d7f72675474d07041fc48d94e5b8
Alexander Walther <github.com@alexplus.de>
2018-05-02 18:57:23 +0200

    Markitup-Profil anlegen

---
+++ install.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `%TABLE_PREFIX%markitup_profiles` (`name`, `description`, `urltype`,`minheight`,`maxheight`,`type`, `markitup_buttons`) VALUES
+('textile_dsgvo',	'Standard MarkItUp-Konfiguration',	'relative',	300,	800,	'textile',	'bold,code,grouplink[file|internal|external|mailto],orderedlist,paragraph,unorderedlist');
