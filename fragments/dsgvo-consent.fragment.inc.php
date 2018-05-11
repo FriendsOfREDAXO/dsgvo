cee515261ca5ce973b9e4afcf97e7f5e7a2af407
Alexander Walther <github.com@alexplus.de>
2018-05-11 17:48:41 +0200

    Minify-Fehler beheben
    Fehlendes `;`,

--- fragments/dsgvo-consent.fragment.inc.php
+++ fragments/dsgvo-consent.fragment.inc.php
@@ -26,10 +26,11 @@
 	}
 	</script>
 	<? if($this->html_padding) { ?>
-		<script>
+	</script>
+	<script>
 			var DsgvoConsentPadding = function (event) {
 				document.body.parentNode.style.padding<?= $this->html_padding ?> = document.querySelectorAll('.dsgvo-cookie_consent')[0].offsetHeight+"px";
-			}
+			};
 			window.onresize = DsgvoConsentPadding;
 			window.onload = DsgvoConsentPadding;
 		</script>
