ca47624a96be371fd565eaa0fded896c2c3da4d6
Alexander Walther <github.com@alexplus.de>
2018-05-15 12:09:00 +0200

    rex_sql_table rausgenommen
    war irgendwie noch in der boot.php drin

--- boot.php
+++ boot.php
@@ -12,20 +12,3 @@ if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
 if (rex_addon::get('cronjob')->isAvailable() && rex_addon::get('phpmailer')->isAvailable() && !rex::isSafeMode()) {
     rex_cronjob_manager::registerType('rex_cronjob_dsgvo_phpmailerlogs');
 }
-
-rex_sql_table::get(rex::getTable('dsgvo_client'))
-->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
-    ->ensureColumn(new rex_sql_column('category', 'text'))
-    ->ensureColumn(new rex_sql_column('keyword', 'text'))
-    ->ensureColumn(new rex_sql_column('domain', 'text'))
-    ->ensureColumn(new rex_sql_column('lang', 'text'))
-    ->ensureColumn(new rex_sql_column('prio', 'int(11)'))
-    ->ensureColumn(new rex_sql_column('name', 'text'))
-    ->ensureColumn(new rex_sql_column('text', 'text'))
-    ->ensureColumn(new rex_sql_column('source', 'text'))
-    ->ensureColumn(new rex_sql_column('source_url', 'text'))
-    ->ensureColumn(new rex_sql_column('code', 'text'))
-    ->ensureColumn(new rex_sql_column('status', 'text'))
-    ->ensureColumn(new rex_sql_column('updatedate', 'timestamp', false, '0000-00-00 00:00:00', 'on update CURRENT_TIMESTAMP'))
-    ->setPrimaryKey('id')
-    ->ensure();
