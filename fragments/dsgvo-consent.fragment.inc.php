e837981b2f55acb4ab9198f7f016baf2e72db244
Alexander Walther <github.com@alexplus.de>
2018-05-20 10:52:23 +0200

    Fragment-Korrektur
    Parameter "html_padding" nicht gesetzt führte zu Fehler.

--- fragments/dsgvo-consent.fragment.inc.php
+++ fragments/dsgvo-consent.fragment.inc.php
@@ -13,29 +13,25 @@
 	DsgvoCookies = Cookies.noConflict();
 
 	function dsgvoConsent(status) {
-		
 		if(status == 1) {
 			DsgvoCookies.set("dsgvo_cookie_consent", 1, { expires: 365 });
 			document.querySelectorAll('.dsgvo-cookie_consent')[0].style.display = 'none';
-			<? if($this->html_padding) { ?>
+			<? if(isset($this->html_padding)) { ?>
 				document.body.style.padding<?= $this->html_padding ?> = "0px";
 			<? } ?>
 		} else {
 			DsgvoCookies.set("dsgvo_cookie_consent", -1, { expires: 365 });
 		}
 	}
-	</script>
-	<? if($this->html_padding) { ?>
-	</script>
-	<script>
+	<? if(isset($this->html_padding)) { ?>
 			var DsgvoConsentPadding = function (event) {
 				document.body.style.padding<?= $this->html_padding ?> = document.querySelectorAll('.dsgvo-cookie_consent')[0].offsetHeight+"px";
 				document.body.style.height = "auto";
 			};
 			window.onresize = DsgvoConsentPadding;
 			window.onload = DsgvoConsentPadding;
-		</script>
 	<? } ?>
+	</script>
 <?= rex_config::get("dsgvo", "dsgvo_consent_css"); ?>
 <? } ?>
 <!-- / dsgvo-consent-fragment -->
