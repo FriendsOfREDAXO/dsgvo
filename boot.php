97b2884d0e5dc37bbfe54cbef2d9864b11b7e433
Alexander Walther <github.com@alexplus.de>
2018-05-30 13:25:58 +0200

    Glückskeks (fortune cookie) eingebaut
    kann dann in der Entwicklerkonsole angezeigt werden.

--- boot.php
+++ boot.php
@@ -12,3 +12,7 @@ if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
 if (rex_addon::get('cronjob')->isAvailable() && rex_addon::get('phpmailer')->isAvailable() && !rex::isSafeMode()) {
     rex_cronjob_manager::registerType('rex_cronjob_dsgvo_phpmailerlogs');
 }
+
+$fortune[] = "„Der Mensch ist dazu geboren, großes zu leisten, wenn er versteht, die DSGVO umzusetzen.“"
+ 
+setcookie("fortune", rex_string::normalize(array_rand($fortune)));
