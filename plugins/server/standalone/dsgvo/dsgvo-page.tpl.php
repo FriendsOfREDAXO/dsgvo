40ad376cc60c129d24028bd2da81a0656362d479
Guido Erlinger <dev@helpy.de>
2018-07-07 00:11:49 +0200

    Replaced all php short tags '<?' with '<?php'.

--- plugins/server/standalone/dsgvo/dsgvo-page.tpl.php
+++ plugins/server/standalone/dsgvo/dsgvo-page.tpl.php
@@ -1,24 +1,24 @@
 <div class="dsgvo-privacy dsgvp-privacy-standalone">
-    <? foreach ($dsgvo_pool as $dsgvo_item) { ?>
+    <?php foreach ($dsgvo_pool as $dsgvo_item) { ?>
     <div class="dsgvo-item" data-dsgvo="<?= $dsgvo_item->keyword ?>">
     <div class="dsgvo-item-title">
         <h2>
-        <? if ($dsgvo_item->source_url) { ?>
+        <?php if ($dsgvo_item->source_url) { ?>
                 <a class="dsgvo-item-source_url" href="<?= $dsgvo_item->source_url ?>" title="<?= $dsgvo_item->source ?>">
                 <?= $this->source ?> <?= $dsgvo_item->source ?>
                 </a>
-        <? } ?>
+        <?php } ?>
         <?= $dsgvo_item->name ?>
 </h1>
         </div>
     <div class="dsgvo-item-body">
-        <? if ( !$dsgvo_item->custom_text ) { ?>
+        <?php if ( !$dsgvo_item->custom_text ) { ?>
             <p class="dsgvo-item-text"><?= html_entity_decode($dsgvo_item->text) ?></p>
-        <? } else { ?>
+        <?php } else { ?>
             <p class="dsgvo-item-custom_text"><?= $dsgvo_item->custom_text ?></p>
-        <? } ?>
+        <?php } ?>
         </div>
-        <? if($_COOKIE["dsgvo_".$dsgvo_item->keyword] != -1) {
+        <?php if($_COOKIE["dsgvo_".$dsgvo_item->keyword] != -1) {
             $display[-1] = "block";
             $display[1] = "none";
         } else {
@@ -26,14 +26,14 @@
             $display[1] = "block";
         }
         ?>
-        <? if ($dsgvo_item->code) { ?>
+        <?php if ($dsgvo_item->code) { ?>
         <div class="uk-card-footer">
             <button style="display: <?= $display[-1]; ?>" data-dsgvo-keyword="<?= $dsgvo_item->keyword ?>" onClick="dsgvoCookie('<?= $dsgvo_item->keyword ?>', -1)"><?= $this->revoke ?></button>
             <button style="display: <?= $display[1]; ?>" data-dsgvo-keyword="<?= $dsgvo_item->keyword ?>" onClick="dsgvoCookie('<?= $dsgvo_item->keyword ?>', 1)"><?= $this->consent ?></button>
         </div>
-        <? } ?>
+        <?php } ?>
     </div>
-    <?
+    <?php
         }
     ?>
 </div>
--- plugins/server/standalone/standalone.php
+++ plugins/server/standalone/standalone.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 require_once($_SERVER['DOCUMENT_ROOT']."/dsgvo/dsgvo.inc.php");
 
