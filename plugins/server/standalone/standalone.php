d4960e4b41738a72469582376775b666c1023e17
Stefan Dannfald <online@dpf.yourweb.de>
2019-05-09 14:30:11 +0200

    Update standalone.php

--- plugins/server/standalone/standalone.php
+++ plugins/server/standalone/standalone.php
@@ -6,13 +6,12 @@ echo getTrackingCodes("de", "example.com");
 
 echo getDsgvoPage("de", "example.com");
 
-$consent['info']       			 = "Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu. Weitere Informationen zu Cookies erhalten Sie in unserer";
-$consent['more']       			 = "Datenschutzerklärung.";
-$consent['dismiss']    			 = "OK";
+$consent['info']             = "Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu. Weitere Informationen zu Cookies erhalten Sie in unserer";
+$consent['more']             = "Datenschutzerklärung.";
+$consent['dismiss']          = "OK";
 $consent['dismiss_callback'] = ""; // bzw. Funktion übergeben "meine_callback_funktion()";
-$consent['url']        			 = "/datenschutz/";
+$consent['url']              = "/datenschutz/";
 
 echo getDsgvoConsent($consent);
-
 
 ?>
