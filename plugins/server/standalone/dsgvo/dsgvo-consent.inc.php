414c03ede2247713696ff17a3e0d52e16c27f09c
Stefan Dannfald <online@dpf.yourweb.de>
2019-05-09 14:26:47 +0200

    Cookie Consent: Callback-Funktion hinzugefügt

--- plugins/server/standalone/dsgvo/dsgvo-consent.inc.php
+++ plugins/server/standalone/dsgvo/dsgvo-consent.inc.php
@@ -1,10 +1,11 @@
 <?php
 
-$consent['info']            = "Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu. Weitere Informationen zu Cookies erhalten Sie in unserer";
-$consent['more']            = "Datenschutzerklärung.";
-$consent['dismiss']         = "OK";
-$consent['url']             = "/datenschutz.php";
-$consent['html_padding']    = "Bottom";
+$consent['info']             = "Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu. Weitere Informationen zu Cookies erhalten Sie in unserer";
+$consent['more']             = "Datenschutzerklärung.";
+$consent['dismiss']          = "OK";
+$consent['dismiss_callback'] = "";  // bzw. Funktion übergeben "meine_callback_funktion()";
+$consent['url']              = "/datenschutz.php";
+$consent['html_padding']     = "Bottom";
 
 echo getDsgvoConsent($consent);
 ?>
